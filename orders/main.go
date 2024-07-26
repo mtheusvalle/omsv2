c76a8056466fb5ef179a08b8494b6328657db846
Matheus Valle <55810334+mtheusvalle@users.noreply.github.com>
2024-07-25 22:57:01 -0300

    feat: add a grpc service for make orders

--- orders/main.go
+++ orders/main.go
@@ -1,10 +1,38 @@
 package main
 
-import "context"
+import (
+	"context"
+	"log"
+	"net"
+
+	common "github.com/mtheusvalle/commons"
+	"google.golang.org/grpc"
+)
+
+var (
+	grpcAddr = common.EnvString("GRPC_ADDR", "localhost:2000")
+)
 
 func main() {
+
+	grpcServer := grpc.NewServer()
+
+	l, err := net.Listen("tcp", grpcAddr)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	defer l.Close()
+
 	store := NewStore()
 	svc := NewService(store)
+	NewGRPCHandler(grpcServer)
 
 	svc.CreateOrder(context.Background())
+
+	log.Println("Starting gRPC server at", grpcAddr)
+
+	if err := grpcServer.Serve(l); err != nil {
+		log.Fatal(err.Error())
+	}
 }
