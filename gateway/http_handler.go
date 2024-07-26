1c3fa5efb5393ce3743b5475d0f67d1404b71e2d
Matheus Valle <55810334+mtheusvalle@users.noreply.github.com>
2024-07-25 22:54:02 -0300

    feat: create a grpc connection with others services of my application

--- gateway/http_handler.go
+++ gateway/http_handler.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"errors"
 	"net/http"
+
+	common "github.com/mtheusvalle/commons"
+	pb "github.com/mtheusvalle/commons/api"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type handler struct {
-	// gateway
+	client pb.OrderServiceClient
 }
 
-func NewHandler() *handler {
-	return &handler{}
+func NewHandler(client pb.OrderServiceClient) *handler {
+	return &handler{client}
 }
 
 func (h *handler) registerRoutes(mux *http.ServeMux) {
@@ -17,5 +23,49 @@ func (h *handler) registerRoutes(mux *http.ServeMux) {
 }
 
 func (h *handler) HandleCreateOrder(w http.ResponseWriter, r *http.Request) {
+	customerID := r.PathValue("customerID")
+	var items []*pb.ItemsWithQuantify
+	if err := common.ReadJSON(r, &items); err != nil {
+		common.WriteError(w, http.StatusBadRequest, err.Error())
+	}
 
+	if err := validateItems(items); err != nil {
+		common.WriteError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	o, err := h.client.CreateOrder(r.Context(), &pb.CreateOrderRequest{
+		CustomerID: customerID,
+		Items:      items,
+	})
+	rStatus := status.Convert(err)
+	if rStatus != nil {
+		if rStatus.Code() != codes.InvalidArgument {
+			common.WriteError(w, http.StatusBadRequest, rStatus.Message())
+			return
+		}
+
+		common.WriteError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	common.WriteJSON(w, http.StatusOK, o)
+}
+
+func validateItems(items []*pb.ItemsWithQuantify) error {
+	if len(items) == 0 {
+		return errors.New("items must have at least one item")
+	}
+
+	for _, i := range items {
+		if i.ID == "" {
+			return errors.New("item ID is required")
+		}
+
+		if i.Quantity <= 0 {
+			return errors.New("item quantity must be greater than 0")
+		}
+	}
+
+	return nil
 }
--- gateway/main.go
+++ gateway/main.go
@@ -6,15 +6,29 @@ import (
 
 	_ "github.com/joho/godotenv/autoload"
 	common "github.com/mtheusvalle/commons"
+	pb "github.com/mtheusvalle/commons/api"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 var (
-	httpAddr = common.EnvString("HTTP_ADDR", ":8080")
+	httpAddr          = common.EnvString("HTTP_ADDR", ":8080")
+	ordersServiceAddr = "localhost:2000"
 )
 
 func main() {
+	conn, err := grpc.Dial(ordersServiceAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatal("Failed to dial server: %v", err)
+	}
+	defer conn.Close()
+
+	log.Println("Dialing orders serice at", ordersServiceAddr)
+
+	c := pb.NewOrderServiceClient(conn)
+
 	mux := http.NewServeMux()
-	handler := NewHandler()
+	handler := NewHandler(c)
 	handler.registerRoutes(mux)
 
 	log.Printf("Starting HTTP server at %s", httpAddr)
